8df8205ffc767ef450d271822f854cabaa78bb2e
Filippo Garota <filogarota101@gmail.com>
2021-03-19 15:55:17 +0100

    primo snack delle bici

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,1 +1,32 @@
+//Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore
 
+const bici = [
+  {
+    nome:'Trek',
+    peso: 5000
+  },
+
+  {
+    nome:'Giant',
+    peso: 8000
+  },
+
+  {
+    nome:'Atala',
+    peso: 3000
+  },
+
+  {
+    nome:'Santa Cruz',
+    peso: 6000,
+  }
+];
+console.log(bici);
+
+let biciLeggera = bici[0];
+for (var i = 0; i < bici.length; i++) {
+  if (bici[i].peso < biciLeggera.peso) {
+    biciLeggera = bici[i];
+    console.log(`${biciLeggera.nome} è la bici più leggera`);
+  }
+}
